cec2f6fc79404288808e6df64b2fdd159e8cf3e3
vvalid <walidwbshazzam@gmail.com>
2026-08-01 18:59:52 +0100

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def isAnagram(x,y):
+            if sorted(x) == sorted(y): return True
+            else: return False
+        output = []
+        myset = set()
+        c = 0
+        for i in range(len(strs)):
+            if "".join(sorted(strs[i])) in myset:
+                for k in range(len(output)):
+                    if isAnagram(strs[i] , output[k][0]):
+                        output[k-c].append(strs[i])
+            else: 
+                output.append([strs[i]])
+                myset.add("".join(sorted(strs[i])))
+                c+=1
+        return output
