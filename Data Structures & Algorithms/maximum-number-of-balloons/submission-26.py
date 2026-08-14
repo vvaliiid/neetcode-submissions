814adb59d67987f3f9baa49f77d34d35e9ecec14
vvalid <walidwbshazzam@gmail.com>
2026-08-14 01:06:46 +0100

    Add: maximum-number-of-balloons - submission-26

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-26.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        seti = Counter(text)
+        seti['l'] = seti['l'] // 2
+        seti['o'] = seti['o'] // 2
+        return int(min(seti[k] for k in ('b','a','l','l','o','o','n') if k in seti)) 
+        
